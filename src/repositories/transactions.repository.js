43f3872f2c80e82263b294de35d86df2dce3ef4c
Gabriel Torres <gbrtorress@gmail.com>
2025-04-07 11:58:56 -0300

    update in repository with new func list

--- src/repositories/transactions.repository.js
+++ src/repositories/transactions.repository.js
@@ -2,12 +2,26 @@ const knex = require('../database/connection')
 
 const create = async(transactionData) => {
     const [transaction] = await knex('transactions')
-    .inster(transactionData)
+    .insert(transactionData)
     .returning('*')
 
     return transaction;
 };
 
+const list = async (month, year) => {
+    let query = knex('transactions');
+
+    if (month && year) {
+        query = query
+        .whereRaw('EXRACT(MONTH FROM created_at) = ?', [month])
+        .andWhereRaw('EXTRACT(YEAR FROM created_at) = ?',[year])
+    }
+
+    const transactions = await query.orderBy('created_at', 'desc');
+    return transactions;
+}
+
 module.exports ={ 
     create,
+    list,
 }; 
